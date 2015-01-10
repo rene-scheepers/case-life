a715e50e6e23caa317a875ecdbe70d0905687091
Anton van Dijk <anton.van.dijk@live.nl>
2015-01-10 12:16:27 +0100

    Set anti-aliasing on 8 samples.

--- desktop/src/com/caselife/game/desktop/DesktopLauncher.java
+++ desktop/src/com/caselife/game/desktop/DesktopLauncher.java
@@ -1,24 +1,25 @@
 package com.caselife.game.desktop;
 
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.caselife.game.CaseLifeGame;
-import org.lwjgl.opengl.*;
-
-import java.awt.*;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
+        // Window.
+        config.title = "Super awesome simulation shizzle";
         config.width = 1920;
         config.height = 1080;
+
+        // FPS.
         config.backgroundFPS = 30;
         config.foregroundFPS = 60;
-        config.title = "Super awesome simulation shizzle";
 
-		new LwjglApplication(new CaseLifeGame(), config);
-	}
+        // Anti-aliasing.
+        config.samples = 8;
+
+        new LwjglApplication(new CaseLifeGame(), config);
+    }
 }
